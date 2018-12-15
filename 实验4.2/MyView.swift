70ce5c55aa816de10a5fad325ed234c15b1fd90a
student <student@iMac-64.local>
2018-12-15 14:27:21 +0800

    Initial commit

---
+++ 实验4.2/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  实验4.2
+//
+//  Created by student on 2018/12/12.
+//  Copyright © 2018年 2016110319. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
